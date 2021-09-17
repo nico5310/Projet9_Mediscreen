0aff2c7507056379484c978f044aa65c5911e180
Nicolas BIANCUCCI <nico5310@gmail.com>
2021-09-17 16:00:42 +0200

    first commit

---
+++ Patient/src/test/java/com/nico5310/Patient/PatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nico5310.Patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
